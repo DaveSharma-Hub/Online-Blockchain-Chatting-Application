6235c83f9f1d177a863e0c5bfb6816a48eae3f90
Dave Sharma <81478885+DaveSharma-Hub@users.noreply.github.com>
2022-01-03 20:33:37 -0700

    Update post.php

--- post.php
+++ post.php
@@ -7,11 +7,12 @@
         $name = $_SESSION['name'];
         $text = $_POST['usermsg'];
         $message = $text;
-    //     $text_message = "<div class='msgln'><span class='chat-time'>".date("g:i A")."</span> <b class='user-name'>".$_SESSION['name']."</b> ".stripslashes(htmlspecialchars($text))."<br></div>";
-    //    file_put_contents("log.html", $text_message, FILE_APPEND | LOCK_EX);
+   
        
-        $conn = new mysqli("localhost","dave(2)","ensf409","blockchain");
-        
+        $username="";
+        $password="";
+        $schema="";
+        $conn = new mysqli("localhost",$username,$password,$schema);        
         if($conn->connect_error){
             die($conn->connect_error);
         }else{
